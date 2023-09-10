10ed8647c98bcf97899d447cdbbb010fbe561ccc
bastianSalinas <bast.s.rojas@gmail.com>
2023-09-10 01:09:28 -0300

    Terminando la ultima actividad de la clase con usuario y rol

--- Actividades Clases/Clase 9/handlebars/src/app.js
+++ Actividades Clases/Clase 9/handlebars/src/app.js
@@ -1,28 +1,26 @@
 const express = require("express")
 const handlebars = require("express-handlebars")
+const viewsRouter = require("./routes/views.router.js")
 const app = express()
 const PORT = 8080
 
 
 app.engine("handlebars", handlebars.engine())
-
 app.set("views", __dirname + "/views")
-
 app.set("view engine", "handlebars")
-
 app.use(express.static(__dirname + "/public"))
 
 //----------------Actividad Clase mostrando usuario random por pantalla-----------//
-const users = [
-    {name: "usuario a", last_name: "apellido usera", age: 20, email: "usera@email.com", phone: 123456},
-    {name: "usuario ab", last_name: "apellido userb", age: 21, email: "userb@email.com", phone: 123456},
-    {name: "usuario abc", last_name: "apellido userc", age: 22, email: "userc@email.com", phone: 123456},
-    {name: "usuario abcd", last_name: "apellido userd", age: 23, email: "userd@email.com", phone: 123456},
-    {name: "usuario abcde", last_name: "apellido usere", age: 24, email: "usere@email.com", phone: 123456}
-]
+// const users = [
+//     {name: "usuario a", last_name: "apellido usera", age: 20, email: "usera@email.com", phone: 123456},
+//     {name: "usuario ab", last_name: "apellido userb", age: 21, email: "userb@email.com", phone: 123456},
+//     {name: "usuario abc", last_name: "apellido userc", age: 22, email: "userc@email.com", phone: 123456},
+//     {name: "usuario abcd", last_name: "apellido userd", age: 23, email: "userd@email.com", phone: 123456},
+//     {name: "usuario abcde", last_name: "apellido usere", age: 24, email: "usere@email.com", phone: 123456}
+// ]
 //-------------------------------Fin Actividad Clase----------------------------//
 
-app.get("/", (req,res) => {
+// app.get("/", (req,res) => {
     // let testUser = {
     //     name: "Bastian",
     //     last_name: "Salinas"
@@ -30,10 +28,12 @@ app.get("/", (req,res) => {
     // res.render("index",testUser)
 
     //----------------Actividad Clase mostrando usuario random por pantalla-----------//
-    const randomIndex = Math.floor(Math.random() * users.length)
-    const randomUser = users[randomIndex]
-    res.render("index", randomUser)
+    // const randomIndex = Math.floor(Math.random() * users.length)
+    // const randomUser = users[randomIndex]
+    // res.render("index", randomUser)
     //-------------------------------Fin Actividad Clase----------------------------//
-})
+// })
+
+app.use("/", viewsRouter)
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`))
---
+++ Actividades Clases/Clase 9/handlebars/src/routes/views.router.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const router = express.Router()
+
+let food = [
+    {name:"Pizza", price: 1000},
+    {name:"Hamburguesa", price: 1500},
+    {name:"Empanadas", price: 4000}
+]
+
+router.get("/", (req, res) => {
+    let testUser = {
+        name: "Alumno A",
+        lastname: "Coder",
+        role: "admin"
+
+        // name: "Alumno A",
+        // lastname: "Coder",
+        // role: "user"
+    }
+    res.render("index", {user: testUser, isAdmin: testUser.role === "admin", food})
+})
+
+module.exports = router
