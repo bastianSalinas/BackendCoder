f8bcfb8da652d3a7bf78ba97b2072a79a64e23af
bastianSalinas <bast.s.rojas@gmail.com>
2023-10-15 00:34:47 -0300

    Se sube actividad de clase 19

---
+++ Actividades Clases/Clase 19/hand on lab/HOLsession-main/src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const session = require("express-session")
+const handlebars = require("express-handlebars")
+const sessionRouter = require("./routes/sessions")
+const viewsRouter = require("./routes/views")
+const MongoStore = require("connect-mongo")
+const mongoose = require("mongoose")
+
+const port = 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://bastsrojas:ptLuitYCTl6wE4jB@cluster0.wx37dwm.mongodb.net/?retryWrites=true&w=majority",
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+        ttl: 1000
+    }),
+    secret: "coderhouse",
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname + "views")
+app.set("view engine", "handlebars")
+
+
+app.use("/api/sessions", sessionRouter)
+app.use("/", viewsRouter)
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+mongoose.connect("mongodb+srv://bastsrojas:ptLuitYCTl6wE4jB@cluster0.wx37dwm.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("Conectado a la base de datos")
+})
+.catch(error => {
+    console.error("Error al conectarse a la base de datos, error"+error)
+})
