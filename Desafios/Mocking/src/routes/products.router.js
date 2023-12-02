14ace85ced096ef53b6a8a169f176e0b88d5d2a8
bastianSalinas <bast.s.rojas@gmail.com>
2023-12-02 01:11:46 -0300

    Se termina el Custom Error para la creación de productos por postman

--- Desafios/Mocking/src/routes/products.router.js
+++ Desafios/Mocking/src/routes/products.router.js
@@ -2,6 +2,9 @@ import { Router } from "express";
 import ProductDTO from "../dao/DTOs/product.dto.js";
 import { productService } from "../repositories/index.js";
 import Products from "../dao/mongo/products.mongo.js"
+import CustomError from "../services/errors/CustomError.js";
+import EErrors from "../services/errors/enum.js";
+import { generateProductErrorInfo } from "../services/errors/info.js";
 
 const router = Router()
 
@@ -11,9 +14,32 @@ router.get("/", async (req, res) => {
     let result = await productMongo.get()
     res.send({ status: "success", payload: result })
 })
+//------------------Info Prueba CUstom Error--------------//
+// http://localhost:8080/products
+// {
+//     "image": "imagen1.jpg",
+//     "stock": 999,
+//     "category": "Electrónicos",
+//     "availability": "in_stock"
+// }
+//--------------------------------------------------------//
 
 router.post("/", async (req, res) => {
     let { description, image, price, stock, category, availability } = req.body
+    const product = { description, image, price, stock, category, availability}
+    if (!description || !price) {
+        try {
+            // Some code that might throw an error
+            throw CustomError.createError({
+                name: 'Error en Creacion de Producto',
+                cause: generateProductErrorInfo(product),
+                message: 'Error al intentar crear el Producto',
+                code: EErrors.REQUIRED_DATA,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
     let prod = new ProductDTO({ description, image, price, stock, category, availability })
     let result = await productService.createProduct(prod)
 })
