732e8b43c82e3328a184e74258058b2a1517f19b
bastianSalinas <bast.s.rojas@gmail.com>
2023-12-23 00:21:28 -0300

    Primera parte documentación API- Usuario terminado

---
+++ Desafios/Documentar API/src/dao/mongo/models/products.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const productsCollection = "products";
+
+const productsSchema = new mongoose.Schema({
+    description: { type: String, max: 100},
+    image: { type: String, max: 100},
+    price: { type: Number},
+    stock: { type: Number},
+    category: { type: String, max: 50 }, // Campo para la categoría
+    availability: { type: String, enum: ['in_stock', 'out_of_stock'] } // Campo para la disponibilidad
+})
+
+const productsModel = mongoose.model(productsCollection, productsSchema)
+
+export default productsModel;
